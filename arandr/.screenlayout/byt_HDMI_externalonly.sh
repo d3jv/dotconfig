9d57e04cde3ebd12e6a8fca0cd7f6da2bb02a2ce
David Štorek <d.storek@seznam.cz>
2026-03-29 20:36:56 +0200

    arandr: Account for monitor resolution and reload wm

--- arandr/.screenlayout/byt_HDMI_externalonly.sh
+++ arandr/.screenlayout/byt_HDMI_externalonly.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output eDP --off --output HDMI-A-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-0 --off
+xrandr --output eDP --off --output HDMI-A-0 --mode 2560x1440 --pos 0x0 --rotate normal --rate 144 --output DisplayPort-0 --off
+
+xrdb -load ~/.Xresources-QHD
+
+herbstclient reload
--- arandr/.screenlayout/naiada_DP_externalonly.sh
+++ arandr/.screenlayout/naiada_DP_externalonly.sh
@@ -3,3 +3,7 @@
 ## Use only external monitor on naiada machines at FI via DP
 
 xrandr --output eDP --off --output HDMI-A-0 --off --output DisplayPort-0 --mode 1920x1200 --pos 0x0 --rotate normal
+
+xrdb -load ~/.Xresources-FHD
+
+herbstclient reload
